d99a64b8b484af2e7ac8a53c25cb633febaba983
VitorNoe <162385647+VitorNoe@users.noreply.github.com>
2025-02-26 20:22:30 -0300

    Delete src directory

--- src/gui/__init__.py
+++
@@ -1,6 +0,0 @@
-from .flights import FlightWindow
-from .passengers import PassengerWindow
-from .bookings import BookingWindow
-from .baggage import BaggageWindow
-from .reports import ReportWindow
-from .settings import SettingsWindow
